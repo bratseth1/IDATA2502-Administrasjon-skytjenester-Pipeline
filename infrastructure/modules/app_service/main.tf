7fecb27a16a3409132a16ae27c22dd40f3c03abc
bratseth1 <52723913+bratseth1@users.noreply.github.com>
2024-11-26 04:04:26 +0100

    d

--- infrastructure/modules/app_service/main.tf
+++ infrastructure/modules/app_service/main.tf
@@ -4,6 +4,7 @@ resource "azurerm_resource_group" "portfolio_resource_group" {
 }
 
 resource "azurerm_application_insights" "app_insights" {
+  depends_on         = [azurerm_resource_group.portfolio_resource_group]
   name                = "${var.app_service_name}-ai"
   location            = var.location
   resource_group_name = azurerm_resource_group.portfolio_resource_group.name
@@ -11,6 +12,7 @@ resource "azurerm_application_insights" "app_insights" {
 }
 
 resource "azurerm_service_plan" "app_service_plan" {
+  depends_on         = [azurerm_resource_group.portfolio_resource_group]
   name                = var.app_service_plan_name
   location            = var.location
   resource_group_name = azurerm_resource_group.portfolio_resource_group.name
@@ -19,6 +21,7 @@ resource "azurerm_service_plan" "app_service_plan" {
 }
 
 resource "azurerm_linux_web_app" "app_service" {
+  depends_on         = [azurerm_resource_group.portfolio_resource_group]
   name                = var.app_service_name
   location            = var.location
   resource_group_name = azurerm_resource_group.portfolio_resource_group.name
